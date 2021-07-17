85fc9ecf4b259f408e1f5545ae60513a76ca5524
Arab Kumar <arabkumar1000@gmail.com>
2021-07-17 12:36:55 +0530

    final changes

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Sports App',
       debugShowCheckedModeBanner: false,
       darkTheme: ThemeData.dark().copyWith(
         primaryColorDark: Colors.white,
